d376a01de97be922c82eeb36e5556b92443ae1e4
M4rYu5 <_>
2021-10-02 11:39:07 +0300

    Added DigitalClock showing the local time into ShellHeader

--- TimeDisplay/TimeDisplay/Views/Pages/AppShell.xaml.cs
+++ TimeDisplay/TimeDisplay/Views/Pages/AppShell.xaml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TimeDisplay.Resources.Localization;
+using TimeDisplay.Services;
+using TimeDisplay.ViewModels;
+using TimeDisplay.Views.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +15,17 @@ namespace TimeDisplay.Views.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AppShell : Shell
     {
+        private ClockDateTimeUpdater clockUpdater;
+
         public AppShell()
         {
+            DigitalClock = new ClockViewModel { Name = AppLocalization.ShellHeaderTimeName, TimeZoneDifferenceToUTC = DateTime.Now - DateTime.UtcNow};
+            clockUpdater = new ClockDateTimeUpdater(100, DigitalClock);
             InitializeComponent();
+            ShellHeaderDigitalClock.BindingContext = DigitalClock;
         }
+
+        private ClockViewModel DigitalClock { get; set; }
+
     }
 }
