4e08ef5cd03916267e1726e607592e376d16dc61
M4rYu5 <_>
2021-12-30 04:24:46 +0200

    Registered the ClockDetailsPage into the shell routes

--- TimeDisplay/TimeDisplay/Views/Pages/AppShell.xaml.cs
+++ TimeDisplay/TimeDisplay/Views/Pages/AppShell.xaml.cs
@@ -19,9 +19,15 @@ namespace TimeDisplay.Views.Pages
 
         public AppShell()
         {
-            DigitalClock = new ClockViewModel { Name = AppLocalization.ShellHeaderTimeName, TimeZoneDifferenceToUTC = DateTime.Now - DateTime.UtcNow};
-            clockUpdater = new ClockDateTimeUpdater(100, DigitalClock);
             InitializeComponent();
+
+            // adding routes
+            // - "times" page is defined in xaml
+            Routing.RegisterRoute("//times/details", typeof(ClockDetailsPage));
+
+            // initialize and set the header's clock view model
+            DigitalClock = new ClockViewModel { Name = AppLocalization.ShellHeaderTimeName, TimeZoneDifferenceToUTC = DateTime.Now - DateTime.UtcNow };
+            clockUpdater = new ClockDateTimeUpdater(100, DigitalClock);
             ShellHeaderDigitalClock.BindingContext = DigitalClock;
         }
 
